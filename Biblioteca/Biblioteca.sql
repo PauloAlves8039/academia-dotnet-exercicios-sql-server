abf19e66b130ae382476c014b7fecdbe6b505f6e
PauloAlves8039 <pj38alves@gmail.com>
2023-11-17 09:34:26 -0300

    Inserindo dados nas tabelas do banco Biblioteca

--- Biblioteca/Biblioteca.sql
+++ Biblioteca/Biblioteca.sql
@@ -35,3 +35,60 @@ CREATE TABLE Livro_Autor (
 	FOREIGN KEY (fk_livro) REFERENCES livros(isbn),
 	FOREIGN KEY (fk_autor) REFERENCES autores(id)
 );
+
+INSERT INTO Categorias 
+VALUES 
+('Terror'),
+('Horror'), 
+('Ficção Cientifica'),
+('Humor'), 
+('Literatura Juvenil');
+
+INSERT INTO Editoras 
+VALUES 
+('Darkside'),
+('Editora Aleph'),
+('Suma'), 
+('Broadway Books'), 
+('Via Leitura'),
+('Clássicos Zahar'),
+('Principis'),
+('Penguin-Companhia');
+
+INSERT INTO Autores
+VALUES 
+('H. P. Lovecraft', 'Americana'),
+('Stephen King', 'Americana'),
+('William Gibson', 'Canadense'), 
+('Lauren Weisberger', 'Americana'), 
+('Antonie de Saint-Exupéry', 'Francesa'), 
+('H. G. Wells', 'Britânica'), 
+('Júlio Verne', 'Francesa'), 
+('Jack London', 'Americana');
+
+INSERT INTO Livros 
+VALUES 
+('6555981083', 'Chamado de Cthulhu', 2021, 1, 1),
+('8576573008', 'Neuromancer', 2016, 2, 2),
+('8560280944', 'It: A coisa', 2014, 3, 1),  
+('0767914767', 'O Diabo Veste Prata', 2004, 4, 4), 
+('856709710X', 'O Pequeno Príncipe', 2021, 5, 5), 
+('8556510094', 'A guerra dos mundos', 2016, 3, 3), 
+('8537815861', 'O Homem Invisível', 2017, 6, 3), 
+('8594318766', 'Vinte mil léguas submarinas', 2019, 7, 3),
+('8594318146', 'A volta ao mundo em 80 dias', 2019, 7, 3),
+('8563560859', 'Caninos brancos', 2014, 8, 5);
+
+
+INSERT INTO Livro_Autor 
+VALUES 
+('6555981083', 1),
+('8560280944', 2),
+('8576573008', 3), 
+('0767914767', 4), 
+('856709710X', 5), 
+('8556510094', 6),
+('8537815861', 6), 
+('8594318766', 7),
+('8594318146', 7),
+('8563560859', 8);
